f1a50f458dfe82fa6275cd0d17a58af78bfa496d
Drouard Ludovic <ludovic.drouard@orange.com>
2022-02-22 09:40:33 +0000

    Remove db connection limits

--- main.go
+++ main.go
@@ -193,16 +193,16 @@ func Init() *sql.DB {
   // Set the maximum number of concurrently open connections (in-use + idle)
   // to 5. Setting this to less than or equal to 0 will mean there is no
   // maximum limit (which is also the default setting).
-  db.SetMaxOpenConns(5)
+//  db.SetMaxOpenConns(5)
 
   // Set the maximum number of concurrently idle connections to 5. Setting this
   // to less than or equal to 0 will mean that no idle connections are retained.
-  db.SetMaxIdleConns(5)
+//  db.SetMaxIdleConns(5)
 
   // Set the maximum lifetime of a connection to 1 hour. Setting it to 0
   // means that there is no maximum lifetime and the connection is reused
   // forever (which is the default behavior)
-  db.SetConnMaxLifetime(5*time.Minute)
+//  db.SetConnMaxLifetime(5*time.Minute)
 
 	return db
 }
